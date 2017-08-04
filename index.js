68afc7de59f440ee2a1ee06305783792c9324c2e
Spencer <spencer@artix.cc>
2017-08-03 21:05:57 -0400

    Add new index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-module.exports = require('./lib/ping.js');
+const pc = require('./lib/pc/ping');
+const pe = require('./lib/pe/ping');
+
+/**
+ * Ping a PC or PE server and return the result as an object.
+ * @param {object} [opts={}] - Options to use.
+ * @param opts.server {number} - The server to ping.
+ * @param {string} [opts.type=pc] - The type of server to ping. This should be pc or pe.
+ * @param {number} [opts.protocol=47] - The PC protocol version to ping with, if using pc.
+ * @param {number} [opts.port] - The port to ping.
+ * @param {number} [opts.timeout] - Time in ms to wait before timing out a ping.
+ * @return {Promise}
+ */
+function ping(opts = {}) {
+    if (opts.type && opts.type.toLowerCase() === 'pe') {
+        return pe(opts);
+    } else {
+        return pc(opts);
+    }
+}
+
+ping.pc = pc;
+ping.pe = pe;
+
+module.exports = ping;
